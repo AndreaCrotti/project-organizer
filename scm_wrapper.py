0ee17575f8bbb90550c2d555d37e1d78ed53916a
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-11-19 22:21:15 +0000

    more possible ideas

--- scm_wrapper.py
+++ scm_wrapper.py
@@ -1,1 +1,19 @@
-# 
+"""
+Module to create a nice generalised interface for
+"""
+
+
+class Commit(object):
+    pass
+
+
+class Repo(object):
+    """
+    A repository is composed by many commits, and we should be able to
+    do some standard operations for each repository
+    """
+    def revision(self):
+        """Get the actual revision of the rpository
+        """
+
+    
